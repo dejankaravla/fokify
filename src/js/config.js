f4ff29b04b31c8dabf5de76907eec5091369ab7c
dejankaravla <karavla013@gmail.com>
2020-12-24 04:13:56 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '225143c6-893b-4b9a-b2de-54de9f6ec973';
+export const MODAL_CLOSE_SEC = 2.5;
